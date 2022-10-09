b0549ae989224c5d810ef1ccdeaf5f297a835ca6
Alex Robu <aleksandr.robu@icloud.com>
2022-10-09 20:49:23 +0300

    Topics + Superuser + Entry

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    """Тема, яку вивчає користувач."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Повернути рядкове представлення моделі."""
+        return self.text
+
+class Entry(models.Model):
+    """Якасть конкретна інформація до цієї теми."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Повертає представленя моделі у string."""
+        return f"{self.text[:50]}..."
