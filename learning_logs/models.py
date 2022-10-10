0a9cb9a668870d891494a6ed314becb632c116a4
Alex Robu <aleksandr.robu@icloud.com>
2022-10-10 19:04:17 +0300

    18_2 upgrate

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -21,4 +21,7 @@ class Entry(models.Model):
 
     def __str__(self):
         """Повертає представленя моделі у string."""
-        return f"{self.text[:50]}..."
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        return f"{self.text[:]}"
+
